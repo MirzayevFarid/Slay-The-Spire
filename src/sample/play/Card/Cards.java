aa7760ac4291d271d8bca9dd8b11e79acb572855
bartuserttas <n.bartuserttas@gmail.com>
2020-04-22 01:25:55 +0300

    Optimization of Cards class ArrayList accesses and iterations.

--- src/sample/play/Card/Cards.java
+++ src/sample/play/Card/Cards.java
@@ -3,27 +3,28 @@ package sample.play.Card;
 import java.util.ArrayList;
 
 public class Cards {
-    private ArrayList<Card> cardList = new ArrayList<Card>();
+    private ArrayList<Card> cards = new ArrayList<Card>();
 
     public Cards(ArrayList<Card> cardList){
         for ( int i = 0 ; i < cardList.size() ; i++ )
-            this.cardList.add(cardList.get(i));
+            this.cards.add(cardList.get(i));
     }
 
     public Cards(Cards cards) {
-        this.cardList = cards.cardList;
+        for ( int i = 0 ; i < cards.getCards().size() ; i++ )
+            this.cards.add(cards.getCards().get(i));
     }
 
-    public ArrayList<Card> getCardList() {
-        return cardList;
+    public ArrayList<Card> getCards() {
+        return cards;
     }
 
     @Override
     public String toString() {
         String output = "";
-        for ( int i = 0 ; i < cardList.size() ; i++ ) {
+        for (int i = 0; i < cards.size() ; i++ ) {
             output += "Cards{" +
-                    "cardList=" + cardList.get(i).toString() +
+                    "cardList=" + cards.get(i).toString() +
                     '}' + "\n";
         }
         return output;
